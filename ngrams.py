f12ae0204658272194508d21e9c91e693789a166
gptacek <gptacek@vassar.edu>
2022-04-21 13:08:59 -0400

    working on n-grams, using nltk

--- ngrams.py
+++ ngrams.py
@@ -1,5 +1,42 @@
+import argparse
+from tokenize import String
+from typing import List
+from nltk import ngrams
+from collections import Counter
+
+def get_ngrams(chords,n):
+    """
+    takes as input a list of chords represented by Counter objects and
+    a value n for the n-gram (2 for bigram, 3 for trigram, etc) and
+    returns a list of all the given n-grams in the document
+    """
+    n_grams = ngrams(chords, n)
+    return n_grams       
+
+
 def main(args):
-    pass
+    ## get list of counters from file
+    chords = [['d','b'],['g','d','c','f#','e'],['f','c','a'],['c','a','e-','f'],['c','d','a','f'],['c','c#','g#','f']]
+    n = 2
+    
+    #print(chords)
+    
+    n_grams = ngrams(chords,n)
+    print(type(n_grams))
+    for grams in n_grams:
+        print(grams)
+
+    
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--chords",
+        "-c",
+        type=argparse.FileType("r"),
+        help="list of Counter objects that represents chords",
+    )
+    args = parser.parse_args()
+
+    main(args)
