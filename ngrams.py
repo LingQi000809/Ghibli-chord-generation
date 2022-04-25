f3bb9ec4bcb6b11b936eabbeedfd795bbfe9cec4
gptacek <gptacek@vassar.edu>
2022-04-25 15:01:02 -0400

    more ngrams updates, waiting for reading whole directory

--- ngrams.py
+++ ngrams.py
@@ -3,53 +3,42 @@ from tokenize import String
 from typing import List
 from nltk import ngrams
 from collections import Counter
+from parse_chords import read_chord_file
+import os
 
-def get_ngrams(chords, n):
-    """
-    takes as input a list of chords represented by Counter objects and
-    a value n for the n-gram (2 for bigram, 3 for trigram, etc) and
-    returns a list of all the given n-grams in the document
-    """
-    n_grams = ngrams(chords, n)
-    return n_grams       
 
-def get_examples(folder):
+def get_examples(dir, n):
     """
+    FIX THIS 
     get_examples will loop through a folder with .txt files (list of list of characters
     representing sequence of chords) for each piece and build a full n-gram Counter set 
     with the following n values:
     2,3,4, and 5 to start  ...
-
-
     """
-    pass
+    # read files from directory using jack's function
+    # pass that into ngrams here
+    n_counter = Counter(ngrams())
+
+    #return the counter
+    return n_counter
+
 
 def main(args):
-    ## get list of counters from file
-    #chords = [['d','b'],['g','d','c','f#','e'],['f','c','a'],['c','a','e-','f'],['c','d','a','f'],['c','c#','g#','f']]
-    #n = 2
-    
-
-    #print(args.chords.readlines())
-
-    chords = []
-    for chord in args.chords.readlines():
-        temp_chord = chord.strip()
-        list_chord = temp_chord.split(" ")
-        chords.append(list_chord)
+    chords = read_chord_file(args.chords)
     print(chords)
-    
 
-    #with open(args.chords, "r") as fp:
-    #    lines = fp.readlines()
-    #    print(lines)
-    
-    #n_grams = ngrams(chords,n)
-    #print(type(n_grams))
-    #for grams in n_grams:
-    #    print(grams)
+    n = 2
 
-    
+    n_grams = ngrams(chords,n)
+    for grams in n_grams:
+        print(grams)
+
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -58,7 +47,13 @@ if __name__ == "__main__":
         "--chords",
         "-c",
         type=argparse.FileType("r"),
-        help="list of Counter objects that represents chords",
+        help="txt file containing list of sets of string that represents chords",
+    )
+    parser.add_argument(
+        "--path",
+        "-p",
+        type=dir_path,
+        help="directory path for reading files",
     )
     args = parser.parse_args()
 
